a3848f34a35a4abd20955744797901de75d8be9f
Ruzimurod2003 <Ruzimurodabdunazarov2003@mail.ru>
2023-03-03 11:52:38 +0500

    create sample Api

---
+++ ChatApp/Program.cs
@@ -0,0 +1,54 @@
+﻿using OpenAI_API;
+using OpenAI_API.Completions;
+using OpenAI_API.Models;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string OPENAI_API_KEY = "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";
+        List<Message> messages = new List<Message>();
+        while (true)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("User: ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            string message = Console.ReadLine();
+            string answer = "";
+            if (message == "quit")
+            {
+                break;
+            }
+            messages.Add(new Message("user", message));
+            OpenAIAPI api = new OpenAIAPI(OPENAI_API_KEY);
+
+            CompletionRequest completion = new CompletionRequest();
+            completion.Prompt = message;
+            completion.Model = Model.DavinciText;
+            completion.MaxTokens = 4000;
+            var result = api.Completions.CreateCompletionAsync(completion);
+            if (result != null)
+            {
+                answer = result.Result.Completions[0].Text;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write("Bot: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(answer);
+                messages.Add(new Message("bot", answer));
+                Console.Write("\n");
+            }
+        }
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
+
+public class Message
+{
+    public Message(string role, string content)
+    {
+        Role = role;
+        Content = content;
+    }
+    public string Role { get; set; }
+    public string Content { get; set; }
+}
